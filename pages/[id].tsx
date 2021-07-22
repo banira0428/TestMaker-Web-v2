c3b9795513d1ed7a3fa6736dc4d52d44085cd128
ymdkit <yamada2021@s.okayama-u.ac.jp>
2021-07-22 14:35:03 +0900

    存在しない問題集の共有リンクを踏んだ際に， 404 に遷移する

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -3,6 +3,7 @@ import Head from "next/dist/next-server/lib/head";
 import QrCodeDialog from "../components/QrCodeDialog";
 import { createDynamicLinks } from "../lib/services/dynamicLinks";
 import { GetServerSideProps } from "next";
+import { getTest } from '../lib/services/firestore';
 
 type PathParams = {
   id: string;
@@ -34,6 +35,14 @@ export default function DynamicLinks<Props>(props) {
 
 export const getServerSideProps: GetServerSideProps<Props> = async context => {
   const { id } = context.params as PathParams
+
+  const test = await getTest(id);
+  if(!test){
+    return {
+      notFound: true
+    };
+  }
+
   const url = await createDynamicLinks(id)
 
   const props: Props = {
