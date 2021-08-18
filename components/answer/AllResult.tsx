8485e2208357ca9e63ec440eaa2f2c860921d1cd
ymdkit <yamada2021@s.okayama-u.ac.jp>
2021-08-18 11:13:25 +0900

    Add: 解答結果をグラフで表示する機能の追加

--- components/answer/AllResult.tsx
+++ components/answer/AllResult.tsx
@@ -1,3 +1,5 @@
+import { Doughnut } from "react-chartjs-2";
+
 type Props = {
   correctNum: number;
   size: number;
@@ -8,9 +10,23 @@ export default function AllResult(props: Props) {
   return (
     <div className="mt-5">
       <p className="font-semibold">総合結果</p>
-      <p className="text-xl mt-3">
-        {props.correctNum}/{props.size}
-      </p>
+      <div className="p-5 max-w-sm mx-auto">
+        <Doughnut
+          data={{
+            labels: ["正解", "不正解"],
+            datasets: [
+              {
+                label: "",
+                data: [props.correctNum, props.size - props.correctNum],
+                backgroundColor: ['rgb(255, 159, 64)', 'rgb(54, 162, 235)'],
+              },
+            ],
+          }}
+        />
+        <p className="text-2xl text-center mt-5">
+          正解率：{props.correctNum}/{props.size}
+        </p>
+      </div>
       <button
         className="block text-white bg-accent p-3 rounded-md w-full mt-5"
         onClick={() => {
